62ac31dfe77318a7f5803bc2af3d13f5bc886993
whenyoubelieve2014 <whenyoubelieve2014@gmail.com>
2014-02-23 01:17:44 -0500

    add postCtrlr

--- NhaList/App_Start/AuthConfig.cs
+++ NhaList/App_Start/AuthConfig.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.Web.WebPages.OAuth;
-using NhaList.Models;
-
-namespace NhaList
+﻿namespace NhaList
 {
     public static class AuthConfig
     {
--- NhaList/Scripts/Controllers/home.js
+++ NhaList/Scripts/Controllers/home.js
@@ -2,11 +2,32 @@
     .module('home', [])
     .controller('homePageCtrlr', function() {})
     .controller('searchCtrlr', [
-        '$scope', '$location',  function($scope, $location) {
+        '$scope', '$location', function($scope, $location) {
             $scope.handleSubmit = function() {
                 if (!$scope.nearBy) return false;
-                
+
                 $location.path('/view/search/' + $scope.nearBy);
+                return true;
+            };
+        }
+    ])
+    .controller('postCtrlr', [
+        '$scope', '$timeout', function ($scope, $timeout) {
+            $scope.handleSubmit = function() {
+                $scope.validating = true;
+                var isComplete = function () {
+                    return $scope.validating && $scope.phone && $scope.email && $scope.nearBy && $scope.text;
+                };
+                var isValid = function() {
+                    return true;
+                };
+                if (!isComplete() || !isValid()) return false;
+                $scope.posting = true;
+
+                $timeout(function() {
+                    $scope.validating = false;
+                    $scope.posted = true;
+                }, 1000);
                 return true;
             };
         }
