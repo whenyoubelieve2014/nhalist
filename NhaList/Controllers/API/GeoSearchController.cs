56eb5795ba82fb2c37a6a25d1fe9e92b6b1f184f
whenyoubelieve2014 <whenyoubelieve2014@gmail.com>
2014-02-23 19:31:05 -0500

    add Entities GeoSearch and Post

---
+++ NhaList/Controllers/API/GeoSearchController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace NhaList.Controllers.API
+{
+    public class GeoSearchController : ApiController
+    {
+        // GET api/GeoSearch
+        public IEnumerable<string> Get()
+        {
+            return new[] {"value1", "value2"};
+        }
+
+        // GET api/GeoSearch/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/GeoSearch
+        public void GeoSearch([FromBody] string value)
+        {
+        }
+
+        // PUT api/GeoSearch/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/GeoSearch/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Sql/Tables/Post.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Post] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Phone]      NVARCHAR (20)  NOT NULL,
+    [Email]      NVARCHAR (50)  NOT NULL,
+    [Address]    NVARCHAR (250) NOT NULL,
+    [Latitude]   FLOAT (53)     NOT NULL,
+    [Longtitude] FLOAT (53)     NOT NULL,
+    [Text]       NVARCHAR (MAX) NOT NULL,
+    [CreatedOn]  DATETIME       CONSTRAINT [DF_Post_EditedOn] DEFAULT (getdate()) NOT NULL,
+    CONSTRAINT [PK_Post] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
