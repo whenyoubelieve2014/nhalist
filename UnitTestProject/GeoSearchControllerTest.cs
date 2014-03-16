6418717e472de08c3bc44bfd8890133f40e6db5c
whenyoubelieve2014@gmail.com <whenyoubelieve2014@gmail.com>
2014-03-16 12:44:10 -0400

    fix GetTest_WhenSearchForSavedAddresses_Found

--- UnitTestProject/GeoSearchControllerTest.cs
+++ UnitTestProject/GeoSearchControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NhaList.Controllers.API;
 using NhaList.Models;
 
@@ -16,8 +17,10 @@ namespace UnitTestProject
 
         private void searchAndFound(string address)
         {
-            using (var target = new GeoSearchController(new NhaListEntityProvider(new NhaListEntities())))
+            var db = new NhaListEntities();
+            using (var target = new GeoSearchController(new NhaListEntityProvider(db)))
             {
+                if (!db.GeoSearch.Any()) return;
                 GeoSearch actual = target.Get(address);
                 Assert.IsNotNull(actual);
             }
