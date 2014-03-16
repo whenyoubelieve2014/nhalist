7e1f8872daeeefd39ae93e3ce0f18ce418d7eda1
whenyoubelieve2014@gmail.com <whenyoubelieve2014@gmail.com>
2014-03-16 10:05:59 -0400

    ignore Sql project

---
+++ NhaList/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyCompany("WhenYouBelieve2014.com")]
+[assembly: AssemblyProduct("NhaList")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("NhaList")]
+[assembly: AssemblyVersion("1.0.0.*")]
