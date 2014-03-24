9e2f49068e622e386c1dc93d59fbcd9578faaddb
whenyoubelieve2014@gmail.com <whenyoubelieve2014@gmail.com>
2014-03-23 21:45:28 -0400

    add analytics

--- NhaList/App_Start/BundleConfig.cs
+++ NhaList/App_Start/BundleConfig.cs
@@ -7,26 +7,27 @@ namespace NhaList
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //    "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+            //    "~/Scripts/jquery-ui-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.unobtrusive*",
-                "~/Scripts/jquery.validate*"));
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //    "~/Scripts/jquery.unobtrusive*",
+            //    "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap*"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //    "~/Scripts/bootstrap*"));
 
             bundles.Add(new ScriptBundle("~/bundles/app")
                 .Include("~/Scripts/Filters/*.js")
                 .Include("~/Scripts/Services/*.js")
                 //.Include("~/Scripts/Controllers/search.js")
                 .Include("~/Scripts/Controllers/*.js")
-                .Include("~/Scripts/appRoutes.js")
-                .Include("~/Scripts/app.js")
+                //.Include("~/Scripts/appRoutes.js")
+                //.Include("~/Scripts/app.js")
+                .Include("~/Scripts/*.js")
                 );
 
             bundles.Add(new StyleBundle("~/Content/site")
